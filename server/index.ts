1ff9cd2390760037407b2449018cdcef388234d4
Bhuvaneshwaran <bhuvaneshboominathan@gmail.com>
2025-10-16 15:00:20 +0530

    Initial commit for Vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -63,8 +63,7 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    // host: "127.0.0.1", // Explicitly use IPv4s
   }, () => {
     log(`serving on port ${port}`);
   });
